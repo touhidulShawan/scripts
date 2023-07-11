d91b47741e8c68352bf29c456d391535e5be1fbd
Touhidul Shawan <touhidulshawan@gmail.com>
2023-07-11 20:30:18 +0600

    make clean and functional

--- bash/advanced_copy.sh
+++ bash/advanced_copy.sh
@@ -12,47 +12,51 @@
 
 echo "Available coreutils here https://ftp.gnu.org/gnu/coreutils/"
 echo "Enter the version of coreutils(ex: 9.1.2): "
-read coreutilVersion
+read -r coreutilVersion
 
 # check if the coreutils.tar file is exist or not
 if [[ -f ./coreutils-${coreutilVersion}.tar.xz ]]; then
-   echo "File already exist" 
+	echo "File already exist"
 else
-    # download selected version of coreutils
-    if command -v xh &> /dev/null; then
-        xh --download "http://ftp.gnu.org/gnu/coreutils/coreutils-${coreutilVersion}.tar.xz"
-    else
-        wget "http://ftp.gnu.org/gnu/coreutils/coreutils-${coreutilVersion}.tar.xz"
-    fi
+	# download selected version of coreutils
+	if command -v xh &>/dev/null; then
+		xh --download "http://ftp.gnu.org/gnu/coreutils/coreutils-${coreutilVersion}.tar.xz"
+	else
+		wget "http://ftp.gnu.org/gnu/coreutils/coreutils-${coreutilVersion}.tar.xz"
+	fi
 fi
-
 
 # extracting tar file of coreutils
-if [[ -f ./coreutils-${coreutilVersion}.tar.xz ]] ; then
-    tar -xvf ./coreutils-${coreutilVersion}.tar.xz
+if [[ -f ./coreutils-${coreutilVersion}.tar.xz ]]; then
+	tar -xvf ./coreutils-"${coreutilVersion}".tar.xz
+	# delete tar file
+	rm ./coreutils-"${coreutilVersion}".tar.xz
 else
-    echo "No file found to extract"
+	echo "No file found to extract"
 fi
 
-# cd to extracted folder
-if [[ -d ./coreutils-${coreutilVersion} ]]; then
-    cd ./coreutils-${coreutilVersion}/
+(
+	# cd to extracted folder
+	if [[ -d ./coreutils-${coreutilVersion} ]]; then
+		cd ./coreutils-"${coreutilVersion}"/ || exit
 
-    # download latest patch of advcpmv (https://github.com/jarun/advcpmv)
-    echo "Available patch here https://github.com/jarun/advcpmv"
-    echo "Enter the version of patch of advcpmv (ex: 9.2): "
-    read patchVersion
-    if command -v xh &> /dev/null ; then
-        xh --download https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.9-${patchVersion}.patch
-    else
-        wget https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.9-${patchVersion}.patch
-    fi
-    # patch
-    patch -p1 -i advcpmv-0.9-${patchVersion}.patch
-    ./configure
-    make
-    sudo cp src/cp /usr/local/bin/cp
-    sudo cp src/mv /usr/local/bin/mv
-else
-    echo "Please extract tar file first"
-fi
+		# download latest patch of advcpmv (https://github.com/jarun/advcpmv)
+		echo "Available patch here https://github.com/jarun/advcpmv"
+		echo "Enter the version of patch of advcpmv (ex: 9.2): "
+		read -r patchVersion
+		if command -v xh &>/dev/null; then
+			xh --download https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.9-"${patchVersion}".patch
+		else
+			wget https://raw.githubusercontent.com/jarun/advcpmv/master/advcpmv-0.9-"${patchVersion}".patch
+		fi
+		# patch
+		patch -p1 -i advcpmv-0.9-"${patchVersion}".patch
+		./configure
+		make
+		sudo cp src/cp /usr/local/bin/cp
+		sudo cp src/mv /usr/local/bin/mv
+	else
+		echo "Please extract tar file first"
+	fi
+)
+rm -rf coreutils-"${coreutilVersion}"
